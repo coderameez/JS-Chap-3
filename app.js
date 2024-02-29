5711e82d3edfc6ece389effa79b78eb0575d2c75
Muhammad Rameez Khan <m.rameezkhan77@gmail.com>
2024-02-29 09:46:39 +0500

    added html and js file

---
+++ app.js
@@ -0,0 +1,18 @@
+// Task 1
+var age = 15;
+alert("I am" + " " + age + " " + "years old");
+
+// Task 2
+var visits = 1;
+alert("You have visited this website" + " " + visits + " " + "times");
+
+// Task 3
+var birthYear = 2006
+document.write("My birthyear is" + " " + birthYear + " ");
+document.write("Datatype of this variable is number" + " ")
+
+// Task 4
+var name = "John Doe"
+var title = "T-Shirts"
+var quantity = 5
+document.write(name + " " + "ordered" + " " + quantity + " " + title + " " + "XYZ Clothing Store");
